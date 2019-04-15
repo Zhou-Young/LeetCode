06fffca999d77f642ce6d96aa5d34a0bb012bb79
zyoung <181772441@qq.com>
2019-04-15 22:15:33 +0800

    add0415
    add 0415

--- 42.trapping-rain-water.js
+++ 42.trapping-rain-water.js
@@ -13,69 +13,46 @@
  *
  * Given n non-negative integers representing an elevation map where the width
  * of each bar is 1, compute how much water it is able to trap after raining.
- * 
- * 
+ *
+ *
  * The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
  * In this case, 6 units of rain water (blue section) are being trapped. Thanks
  * Marcos for contributing this image!
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input: [0,1,0,2,1,0,1,3,2,1,2,1]
  * Output: 6
- * 
+ *
  */
 /**
+ * 有四种方法，详情可见网站
+ * 用栈
+ * 只要当前元素比前一个大，就灌水，看灌一次水能淹多少个格子
+ * 然后再灌水
+ * 一排一排的灌水、
+ * 栈底元素是最大的元素
  * @param {number[]} height
  * @return {number}
  */
 var trap = function (height) {
   let stack = [];
-  height.push(0);
-  flag = 0; //0 > ; 1 <
-  sum = 0;
-  lastHeight = 0;
-  count = 0;
-  console.log(height.length - 1);
-  lastSum = 0;
-  if (height.length < 3) {
-    return 0
-  }
-  for (let i = 0; i < height.length - 1; i++) {
-    stack.push(height[i]);
-    if ((i < height.length - 2 && lastHeight > height[i]) || (i == height.length - 2 && height[i + 1] < height[i] && flag != 1)) {
-      sum = sum + lastHeight - height[i]
-    }
-    count++;
-
-    if (height[i + 1] > height[i] && flag != 0) {
-      console.log(height[i], '制低点')
-      flag = 0;
-    } else if (height[i + 1] < height[i] && flag != 1) {
-      // 制高点
-      console.log(height[i], '制高点')
-      flag = 1;
-      if (lastHeight > height[i]) {
-        sum = lastSum;
-        lastHeight = height[i]
-        console.log(i, count, stack, '--')
-        stack.splice(i - count, count);
-        console.log(stack)
-        for (let j = i - count; j <= i; j++) {
-          stack.push(height[j]);
-          if (lastHeight > height[j]) {
-            sum = sum + lastHeight - height[j]
-          }
-        }
-      } else {
-        lastSum = sum;
-        count = 0;
-        lastHeight = height[i]
+  let current = 0;
+  let ans = 0;
+  while (current < height.length) {
+    console.log(current, height[current], stack.length, height[stack[stack.length - 1]])
+    while (stack.length && height[current] > height[stack[stack.length - 1]]) { //当前元素比栈顶元素大，则栈顶元素出栈，计算值，若当前元素比栈顶元素小，则入栈
+      let top = stack[stack.length - 1];
+      stack.pop();
+      if (!stack.length) {
+        break;
       }
-
+      let distance = current - stack[stack.length - 1] - 1;
+      let bounded_height = Math.min(height[current], height[stack[stack.length - 1]]) - height[top];
+      ans += distance * bounded_height;
     }
+    stack.push(current++);
   }
-  console.log(sum)
-  return sum;
+  return ans;
 };
