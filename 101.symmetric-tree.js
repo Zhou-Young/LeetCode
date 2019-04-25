ed1ad0b3699d5a06153e75050beb07888551c999
zhouyang <181772441@qq.com>
2019-04-25 08:53:47 +0800

    add0423
    add0423

---
+++ 101.symmetric-tree.js
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=101 lang=javascript
+ *
+ * [101] Symmetric Tree
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (43.13%)
+ * Total Accepted:    385.8K
+ * Total Submissions: 893.2K
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ *
+ *
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ *
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ *
+ *
+ *
+ * But the following [1,2,2,null,3,null,3]  is not:
+ *
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ *
+ *
+ *
+ *
+ * Note:
+ * Bonus points if you could solve it both recursively and iteratively.
+ *
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  // 递归
+  function check(node1, node2) {
+    if (!node1 && !node2) {
+      return true;
+    }
+    if (!node1 || !node2) {
+      return false;
+    }
+    if (node1.val !== node2.val) {
+      return false;
+    }
+    return check(node1.left, node2.right) && check(node1.right, node2.left);
+  }
+  return check(root, root);
+};
--- 99.recover-binary-search-tree.js
+++ 99.recover-binary-search-tree.js
@@ -12,56 +12,56 @@
  * Testcase Example:  '[1,3,null,null,2]'
  *
  * Two elements of a binary search tree (BST) are swapped by mistake.
- * 
+ *
  * Recover the tree without changing its structure.
- * 
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: [1,3,null,null,2]
- * 
+ *
  * 1
  * /
  * 3
  * \
  * 2
- * 
+ *
  * Output: [3,1,null,null,2]
- * 
+ *
  * 3
  * /
  * 1
  * \
  * 2
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: [3,1,4,null,null,2]
- * 
+ *
  * ⁠ 3
  * ⁠/ \
  * 1   4
- * /
- * 2
- * 
+ *    /
+ *   2
+ *
  * Output: [2,1,4,null,null,3]
- * 
+ *
  * ⁠ 2
  * ⁠/ \
  * 1   4
- * /
- * ⁠ 3
- * 
- * 
+ *    /
+ * ⁠  3
+ *
+ *
  * Follow up:
- * 
- * 
+ *
+ *
  * A solution using O(n) space is pretty straight forward.
  * Could you devise a constant space solution?
- * 
- * 
+ *
+ *
  */
 /**
  * Definition for a binary tree node.
@@ -75,6 +75,25 @@
  * @return {void} Do not return anything, modify root in-place instead.
  */
 var recoverTree = function(root) {
-    
+  let prev = new TreeNode(-Number.MAX_VALUE); // 最小数
+  let node1, node2; // 第一个错误的点和第二个错误的点   63452   第一个错的是前一个，第二个错的是后一个
+  //中序遍历
+  function traverse(root) {
+    if (root) {
+      traverse(root.left);
+      if (root.val < prev.val) {
+        if (!node1) {
+          node1 = prev;
+        }
+        node2 = root;
+      }
+      prev = root;
+      traverse(root.right);
+    }
+  }
+  traverse(root);
+  // 找到错误点后，交换他们的值就好了，改node相当于改root
+  let temp = node1.val;
+  node1.val = node2.val;
+  node2.val = temp;
 };
-
